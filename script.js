ab6ef7f72db48b2bc861ea18561855f057ae725a
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-05-11 00:18:00 +0530

    added necessary

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
+let SavingsAmount = 0;
+let ExpenseAmount = 0;
+let InvestmentAmount = 0;
 const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const select = document.querySelector("select");
 const amount = document.getElementById("amount");
-const balance = document.getElementById("balance");
 const totalAmount = document.querySelector(".totalAmount");
+const toggleHistory = document.querySelectorAll("body img");
 const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>`;
 
-const canvas = document.querySelector("canvas");
+const show = () => document.body.classList.toggle("show");
 
-new Chart("myChart", {
+toggleHistory.forEach((toggle) => {
+  toggle.addEventListener("click", show);
+});
+
+let doughnut = new Chart("myChart", {
   type: "doughnut",
   data: {
     datasets: [
       {
-        data: [200, 100, 100],
+        data: [100, 100, 100],
         backgroundColor: [
           "rgb(255, 99, 132)",
           "rgb(54, 162, 235)",
           "rgb(255, 205, 86)",
         ],
         hoverOffset: 4,
-        borderRadius: 30,
+        borderRadius: 28,
         spacing: 10,
       },
     ],
   },
   options: {
-    cutout: 115,
+    cutout: 144,
+    aspectRatio: 1.05,
   },
 });
 
@@ -38,47 +46,56 @@ const localStorageTransactions = JSON.parse(
 let transactions =
   localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
-//5
 //Add Transaction
 function addTransaction(e) {
   e.preventDefault();
   if (text.value.trim() === "" || amount.value.trim() === "") {
     alert("please add text and amount");
   } else {
+    selectValues();
+    const chartDataValues = [InvestmentAmount, ExpenseAmount, SavingsAmount];
+
     const transaction = {
       id: generateID(),
       text: text.value,
       amount: +amount.value,
+      chartValues: chartDataValues,
     };
 
+    updateChart(transaction);
     transactions.push(transaction);
-
     addTransactionDOM(transaction);
+
     updateValues();
     updateLocalStorage();
+
     text.value = "";
     amount.value = "";
   }
 }
 
-//5.5
-//Generate Random ID
-function generateID() {
-  return Math.floor(Math.random() * 1000000000);
+function updateChart(transaction) {
+  doughnut.data.datasets[0].data = transaction.chartValues;
+  doughnut.update();
+  updateLocalStorage();
 }
 
-//2
+const generateID = () => Math.floor(Math.random() * 1000000000);
+
+function selectValues() {
+  if (select.value === "Investment") {
+    InvestmentAmount += +amount.value;
+  } else if (select.value === "Expense") {
+    ExpenseAmount += +amount.value;
+  } else {
+    SavingsAmount += +amount.value;
+  }
+}
 
 //Add Trasactions to DOM list
 function addTransactionDOM(transaction) {
   const item = document.createElement("li");
-  if (select.value === "Investment") {
-    item.classList.add("investment");
-  } else if (select.value === "Expense") {
-    item.classList.add("Expense");
-  } else {
-    item.classList.add("Savings");
-  }
+
   item.innerHTML = `
   ${svg}<span> ${transaction.text}</span>`;
   list.appendChild(item);
@@ -90,24 +107,12 @@ function addTransactionDOM(transaction) {
   };
 }
 
-//4
-
 //Update the balance income and expence
 function updateValues() {
   const amounts = transactions.map((transaction) => transaction.amount);
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-  // const income = amounts
-  //   .filter((item) => item > 0)
-  //   .reduce((acc, item) => (acc += item), 0)
-  //   .toFixed(2);
-  // const expense = (
-  //   amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
-  //   -1
-  // ).toFixed(2);
   totalAmount.innerHTML = `₹${Math.abs(total)}`;
 }
-
-//6
 
 //Remove Transaction by ID
 function removeTransaction(id) {
@@ -116,18 +121,17 @@ function removeTransaction(id) {
   updateLocalStorage();
   Init();
 }
-//last
+
 //update Local Storage Transaction
 function updateLocalStorage() {
   localStorage.setItem("transactions", JSON.stringify(transactions));
 }
 
-//3
-
 //Init App
 function Init() {
   list.innerHTML = "";
   transactions.forEach(addTransactionDOM);
+  transactions.forEach(updateChart);
   updateValues();
 }
 
